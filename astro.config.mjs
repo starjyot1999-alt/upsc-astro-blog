de08df9273c4d7bdab86d707e16c78fc8f5f6601
Prajyot Shendage <starjyot1999@gmail.com>
2026-07-06 23:01:56 +0530

    Refactor blog page, add responsive about page, and custom 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://my-upsc-prep-hub.com',
+  site: 'https://starjyot1999-alt.github.io/upsc-astro-blog/',
   base: '/upsc-astro-blog',
   integrations: [mdx(), sitemap()],
 });
